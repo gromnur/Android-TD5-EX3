b11c076f97a03770f9c72793a310e59b5263ef93
Flavien Regis <flavienregis@gmail.com>
2018-12-19 14:59:56 +0100

    Ajout code java onglet

--- app/src/main/java/iut/flavienregis/android_td5_ex3/MainActivity.java
+++ app/src/main/java/iut/flavienregis/android_td5_ex3/MainActivity.java
@@ -2,12 +2,32 @@ package iut.flavienregis.android_td5_ex3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends Activity {
+
+    private TabHost lesOnglets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lesOnglets = (TabHost) findViewById(R.id.tableOnglet);
+        lesOnglets.setup();
+
+        // On ajoute les onglet
+        TabHost.TabSpec specification = lesOnglets.newTabSpec("Sélection");
+        specification.setIndicator(getResources().getString(R.string.onglet_selection));
+        specification.setContent(R.id.selection_onglet);
+        lesOnglets.addTab(specification);
+
+        lesOnglets.addTab(lesOnglets.newTabSpec("Correction")
+                            .setIndicator(getResources().getString(R.string.onglet_correction))
+                            .setContent(R.id.correction_onglet));
+
+
+        lesOnglets.setCurrentTab(0);
+
     }
 }
