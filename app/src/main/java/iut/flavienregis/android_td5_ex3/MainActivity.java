dd9d4922933ba31e45f69431140c27ee5fe5f0a6
Flavien Regis <flavienregis@gmail.com>
2019-01-07 11:58:56 +0100

    Ajout spinner

--- app/src/main/java/iut/flavienregis/android_td5_ex3/MainActivity.java
+++ app/src/main/java/iut/flavienregis/android_td5_ex3/MainActivity.java
@@ -1,18 +1,41 @@
 package iut.flavienregis.android_td5_ex3;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TabHost;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends ListActivity {
+
+    final public int NB_MOT_SELECT = 10;
 
     private TabHost lesOnglets;
+
+    private ArrayList<String> motSelect;
+
+    private SelectionMot lesMots = new SelectionMot();
+
+    private ArrayList<String> motAfficher;
+
+    private ArrayAdapter<String> adaptateurMotCorrect;
+
+    private ArrayAdapter<String> adaptateurListeMot;
+
+    private Spinner spinnerListeMotCorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Les onglet
         lesOnglets = (TabHost) findViewById(R.id.tableOnglet);
         lesOnglets.setup();
 
@@ -29,5 +52,54 @@ public class MainActivity extends Activity {
 
         lesOnglets.setCurrentTab(0);
 
+        // La liste
+        motAfficher = new ArrayList<>();
+        adaptateurListeMot = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, motAfficher);
+        setListAdapter(adaptateurListeMot);
+        refreshListe();
+
+        // Spinner
+
+        spinnerListeMotCorrect = (Spinner) findViewById(R.id.listCorrect);
+        motSelect = new ArrayList<>();
+        adaptateurMotCorrect = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,motSelect);
+        spinnerListeMotCorrect.setAdapter(adaptateurMotCorrect);
+
+    }
+
+    public void refreshListe() {
+
+        motAfficher = lesMots.selectionnerMot(NB_MOT_SELECT);
+        adaptateurListeMot.clear();
+        adaptateurListeMot.addAll(motAfficher);
+
+    }
+
+    public void Valider(View view) {
+        SparseBooleanArray elementCoche = getListView().getCheckedItemPositions();
+        motSelect = new ArrayList<>();
+        for (int i = 0; i < elementCoche.size(); i++) {
+            if (elementCoche.valueAt(i)){
+                motSelect.add(getListView().getAdapter().getItem(elementCoche.keyAt(i)).toString());
+            }
+        }
+
+        Toast.makeText(getApplicationContext(), Arrays.toString(motSelect.toArray()), Toast.LENGTH_LONG).show();
+
+        // remplir le spinner
+        adaptateurMotCorrect.clear();
+        adaptateurMotCorrect.addAll(motSelect);
+
+    }
+
+    public void Annuler(View view)  {
+        getListView().clearChoices();
+        getListView().requestLayout();
+        adaptateurMotCorrect.clear();
+    }
+
+    public void Reinitiliser(View view)  {
+        refreshListe();
+        adaptateurMotCorrect.clear();
     }
 }
